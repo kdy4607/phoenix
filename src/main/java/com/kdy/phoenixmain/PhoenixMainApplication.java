4bfd60dd00a67883922e36ed51ecf00c9daac6da
kdy <kdy4607@gmail.com>
2025-07-07 11:54:02 +0900

    commit

---
+++ src/main/java/com/kdy/phoenixmain/PhoenixMainApplication.java
@@ -0,0 +1,13 @@
+package com.kdy.phoenixmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoenixMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoenixMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kdy/phoenixmain/PhoenixMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdy.phoenixmain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoenixMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
