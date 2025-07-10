657cc8378db4239fa51f50fc0ec976e5fc195918
One-o-Five <macho2014@naver.com>
2025-07-11 01:21:24 +0900

    fix loginVO

--- src/main/java/com/kdy/phoenixmain/vo/LoginVO.java
+++ src/main/java/com/kdy/phoenixmain/vo/LoginVO.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.util.Date;
 
@@ -15,6 +16,9 @@ public class LoginVO {
     private String u_id;        // INT에서 VARCHAR2(20)로 변경
     private String u_pw;        // VARCHAR2(100)
     private String u_name;      // VARCHAR2(50)
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")  // 날짜 포맷 지정
     private Date u_birth;       // DATE
+
     private String u_address;   // VARCHAR2(500)
 }
