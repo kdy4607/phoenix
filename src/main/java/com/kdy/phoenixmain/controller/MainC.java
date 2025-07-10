10e47521f9c519db14ab03b7362610c0e5ac446d
ungraduated <85732427+ungraduated@users.noreply.github.com>
2025-07-10 15:43:14 +0900

    fix MainC

--- src/main/java/com/kdy/phoenixmain/controller/MainC.java
+++ src/main/java/com/kdy/phoenixmain/controller/MainC.java
@@ -6,12 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainC {
 
-    @GetMapping("/")
-    public String index() {
-        return "index";
-    }
-
-
     @GetMapping("/credits")
     public String credits() {
         return "credits";
