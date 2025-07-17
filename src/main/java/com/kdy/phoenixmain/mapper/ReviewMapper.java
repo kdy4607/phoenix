a8225bfdda1349a5293148e90eb316cb874f3d9d
(한생명) <(gkstodaud123@gmail.com)>
2025-07-17 11:20:34 +0900

    feat: implement review writing and submission on movie detail page

---
+++ src/main/java/com/kdy/phoenixmain/mapper/ReviewMapper.java
@@ -0,0 +1,36 @@
+package com.kdy.phoenixmain.mapper;
+
+import com.kdy.phoenixmain.vo.ReviewVO;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+
+@Mapper
+    public interface ReviewMapper {
+    @Insert("""
+        INSERT INTO reviews (movie_id, u_id, r_rating, r_text, r_date)
+        VALUES (#{movie_id}, #{u_id}, #{r_rating}, #{r_text}, SYSDATE)
+    """)
+    void insertReview(ReviewVO review);
+
+    @Select("""
+        SELECT * FROM reviews
+        WHERE movie_id = #{movie_id}
+        ORDER BY r_date DESC
+    """)
+    List<ReviewVO> getReviewsByMovieId(int movie_id);
+
+    @Update("""
+        UPDATE reviews
+        SET r_rating = #{r_rating}, r_text = #{r_text}
+        WHERE r_id = #{r_id} AND u_id = #{u_id}
+    """)
+    void updateReview(ReviewVO review);
+
+    @Delete("""
+        DELETE FROM reviews
+        WHERE r_id = #{r_id} AND u_id = #{u_id}
+    """)
+    void deleteReview(@Param("r_id") int r_id, @Param("u_id") String u_id);
+    }
+
---
+++ src/main/java/com/kdy/phoenixmain/vo/ReviewVO.java
@@ -0,0 +1,16 @@
+package com.kdy.phoenixmain.vo;
+
+import lombok.Data;
+
+import java.util.Date;
+@Data
+public class ReviewVO {
+        private int r_id;
+        private int movie_id;
+        private String u_id;
+        private int r_rating;
+        private String r_text;
+        private Date r_date;
+
+
+}
